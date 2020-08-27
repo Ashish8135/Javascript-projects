2ba7a09e007b15694bd45bb6ed0261ae1e8278c1
ashish8135 <ashish.shaivya10@gmail.com>
2020-08-28 02:36:48 +0530

    Digital Clock Completed

---
+++ digital-clock/clock.js
@@ -0,0 +1,30 @@
+setInterval(function(){
+	var currentTime=new Date();
+	var  hours=currentTime.getHours();
+	var minutes=currentTime.getMinutes();
+	var second=currentTime.getSeconds();
+	// var milisecond=currentTime.getMilliseconds();
+	var period="AM";
+
+	if (hours >= 12){
+		period="PM"
+		}
+
+		if (hours >12) {
+			hours=hours-12;
+		}
+		else{
+			hours="0"+hours;
+		}
+
+		if (second< 10) {
+			second="0"+second;
+		}
+		if (minutes <10){
+			minutes="0"+minutes;
+		}
+
+		var clocktime=hours + ":" + minutes + ":" + second + "  " + period;
+		var clock =document.getElementById('clock');
+		clock.innerText=clocktime;
+},1000);
