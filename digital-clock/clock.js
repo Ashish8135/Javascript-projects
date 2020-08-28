827433e3687d9f8ff473acedef38c6eac955fc69
ashish8135 <ashish.shaivya10@gmail.com>
2020-08-28 23:59:16 +0530

    removed comment from digital file

--- digital-clock/clock.js
+++ digital-clock/clock.js
@@ -1,36 +1,30 @@
-// setInterval(function(){
-// 	var currentTime=new Date();
-// 	var  hours=currentTime.getHours();
-// 	var minutes=currentTime.getMinutes();
-// 	var second=currentTime.getSeconds();
-// 	// var milisecond=currentTime.getMilliseconds();
-// 	var period="AM";
+setInterval(function(){
+	var currentTime=new Date();
+	var  hours=currentTime.getHours();
+	var minutes=currentTime.getMinutes();
+	var second=currentTime.getSeconds();
+	// var milisecond=currentTime.getMilliseconds();
+	var period="AM";
 
-// 	if (hours >= 12){
-// 		period="PM"
-// 		}
+	if (hours >= 12){
+		period="PM"
+		}
 
-// 		if (hours >12) {
-// 			hours=hours-12;
-// 		}
-// 		else{
-// 			hours="0"+hours;
-// 		}
+		if (hours >12) {
+			hours=hours-12;
+		}
+		else{
+			hours="0"+hours;
+		}
 
-// 		if (second< 10) {
-// 			second="0"+second;
-// 		}
-// 		if (minutes <10){
-// 			minutes="0"+minutes;
-// 		}
+		if (second< 10) {
+			second="0"+second;
+		}
+		if (minutes <10){
+			minutes="0"+minutes;
+		}
 
-// 		var clocktime=hours + ":" + minutes + ":" + second + "  " + period;
-// 		var clock =document.getElementById('clock');
-// 		clock.innerText=clocktime;
-// },1000);
-
-var divs = document.getElementsByTagName("div");
-
-	for ( var i = 0; i<divs.length; i++ ) {
-		divs[i].appendChild(document.createElement("div"));
-	}
+		var clocktime=hours + ":" + minutes + ":" + second + "  " + period;
+		var clock =document.getElementById('clock');
+		clock.innerText=clocktime;
+},1000);
