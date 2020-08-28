17382b6a10b7b2e7ed608f5d47f54463f1f388c9
ashish8135 <ashish.shaivya10@gmail.com>
2020-08-29 00:41:37 +0530

    tip calculator completed

---
+++ Tip Calculator/tipcalc.js
@@ -0,0 +1,17 @@
+
+function calculate(){
+	var amount = $('#amount').val();
+
+	var percentage = $('#percentage').val();
+
+	var tip=amount * (percentage / 100);
+
+	total = Number(amount) + tip ;
+
+	$('#tip').val(tip.toFixed(2));
+		$('#total').val(total.toFixed(2));
+
+		return false;
+	}
+$('#tip-cal').submit( calculate);
+
